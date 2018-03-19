d6c0a6b0aef0b92392297925bedd20df2e3de123
Alvis Duong <alvisduong@gmail.com>
2018-03-19 17:54:31 +0000

    Install enzyme and test previous changes
    I am a fan of enzyme's way of testing rendered components and its
    selectors.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders with enzyme', () => {
+  const wrapper = shallow(<App />);
+  const h1 = wrapper.find('h1');
+  expect(h1.contains('Checkbox with label')).toEqual(true);
 });
